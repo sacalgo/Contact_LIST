259e268327d7b45d799bc0ad13950f3e709f48fa
Sachin Toppa <algorithms2019@gmail.com>
2021-04-07 07:38:02 +0530

    added authContext and State

--- client/src/App.js
+++ client/src/App.js
@@ -6,23 +6,26 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
-import ContactState from './context/contact/ContactState'
+import ContactState from './context/contact/ContactState';
+import AuthState from './context/auth/AuthState';
 
 const  App=() =>{
   return (
-    <ContactState>
-      <Router>
-        <Fragment>
-          <Navbar></Navbar>
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={Home}></Route>
-              <Route exact path="/about" component={About}></Route>
-            </Switch>
-          </div>
-        </Fragment>
-      </Router>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <Router>
+          <Fragment>
+            <Navbar></Navbar>
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Home}></Route>
+                <Route exact path="/about" component={About}></Route>
+              </Switch>
+            </div>
+          </Fragment>
+        </Router>
+      </ContactState>
+    </AuthState>
   );
 }
 
---
+++ client/src/context/auth/AuthContext.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+import { createContext } from 'react';
+
+const authContext=createContext();
+
+export default authContext;
+
+
