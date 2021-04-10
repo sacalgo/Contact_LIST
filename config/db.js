6551c673e184d4d1c16bf90057c1200b743427a5
Sachin Toppa <algorithms2019@gmail.com>
2021-04-10 16:39:14 +0530

    setting up .env file

--- config/db.js
+++ config/db.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
 const config = require("config");
-
-let db = "mongodb://127.0.0.1/ContactDB";
-
+// const DB =
+//   "mongodb+srv://sacmongo:Sacdb@786@test.ukunm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const db =config.get('mongoURI');
+// console.log(db);
 const connectDB = async () => {
   try {
     await mongoose.connect(db, {
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express=require('express');
 const connectDB=require('./config/db');
 const cookieParser=require('cookie-parser');
 const cors=require('cors');
+require("dotenv").config();
 
 
 const app=express();
