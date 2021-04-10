69cc8bde771c2b19fa1c19de4a312b5d9151c808
Sachin Toppa <algorithms2019@gmail.com>
2021-04-10 17:07:12 +0530

    connected addContact with backend

--- client/src/context/contact/ContactReducer.js
+++ client/src/context/contact/ContactReducer.js
@@ -8,6 +8,7 @@ import {
   UPDATE_CONTACT,
   FILTER_CONTACTS,
   CLEAR_FILTER,
+  CONTACT_ERROR,
 } from "../types";
 
 export default (state, action) => {
@@ -54,7 +55,12 @@ export default (state, action) => {
         ...state,
         filtered: null,
       };
+    case CONTACT_ERROR:
+      return{
+        ...state,
+        error:action.payload
 
+    }
     default:
       return state;
   }
--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -4,6 +4,7 @@ import React, { useReducer } from "react";
 import { v4 as uuid } from "uuid";
 import ContactContext from "./ContactContext";
 import ContactReducer from "./ContactReducer";
+import axios from "axios";
 
 import {
   ADD_CONTACT,
@@ -13,42 +14,34 @@ import {
   UPDATE_CONTACT,
   FILTER_CONTACTS,
   CLEAR_FILTER,
+  CONTACT_ERROR
 } from "../types";
 
 const ContactState = (props) => {
   const initialState = {
-    contacts: [
-      {
-        id: 1,
-        name: "Sac Top",
-        email: "sac@gmail.com",
-        phone: "111-111-1111",
-        type: "personal",
-      },
-      {
-        id: 2,
-        name: "Sara Watson",
-        email: "sara@gmail.com",
-        phone: "222-222-2222",
-        type: "personal",
-      },
-      {
-        id: 3,
-        name: "Harry White",
-        email: "harry@gmail.com",
-        phone: "333-333-3333",
-        type: "professional",
-      },
-    ],
+    contacts: [],
     current: null,
     filtered: null,
+    error:null
   };
   const [state, dispatch] = useReducer(ContactReducer, initialState);
 
   //Add Contact
-  const addContact = (contact) => {
-    contact.id = uuid();
-    dispatch({ type: ADD_CONTACT, payload: contact });
+  const addContact = async (contact) => {
+    // contact.id = uuid(); this was dummy id
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    try {
+      const res = await axios.post("/api/contacts", contact, config);
+
+      //  dispatch({ type: ADD_CONTACT, payload: contact });cleint
+      dispatch({ type: ADD_CONTACT, payload: res.data });
+    } catch (err) {
+      dispatch({ type: CONTACT_ERROR, payload: err.response.msg });
+    }
   };
 
   //Delete Contact
@@ -77,23 +70,24 @@ const ContactState = (props) => {
   };
 
   //Clear Filter
-   const clearFilter = () => {
-     dispatch({ type: CLEAR_FILTER });
-   };
+  const clearFilter = () => {
+    dispatch({ type: CLEAR_FILTER });
+  };
 
   return (
     <ContactContext.Provider
       value={{
         contacts: state.contacts,
         current: state.current,
-        filtered:state.filtered,
-        addContact,
+        filtered: state.filtered,
+        error:state.error,
+         addContact,
         deleteContact,
         setCurrent,
         clearCurrent,
         updateContact,
         filterContacts,
-        clearFilter
+        clearFilter,
       }}
     >
       {props.children}
