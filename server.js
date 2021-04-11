dbd2349775b2e2939e7455c1d0463c1bd90f68b3
Sachin Toppa <algorithms2019@gmail.com>
2021-04-11 18:02:06 +0530

    prepare for deploy

--- server.js
+++ server.js
@@ -1,33 +1,37 @@
-const express=require('express');
-const connectDB=require('./config/db');
-const cookieParser=require('cookie-parser');
-const cors=require('cors');
+const express = require("express");
+const connectDB = require("./config/db");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 require("dotenv").config();
+const path = require("path");
 
-
-const app=express();
+const app = express();
 
 //Connect Database
 connectDB();
 
-
 //Init Middleware
- app.use(express.json({extended:false}));
+app.use(express.json({ extended: false }));
 
-app.get('/', (req, res)=>res.json({msg:'welcome to ContactKeeper API...'}));
-
-
-
+app.get("/", (req, res) =>
+  res.json({ msg: "welcome to ContactKeeper API..." })
+);
 
 //Define Routes
 
-app.use('/api/users', require('./routes/users'));
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/contacts', require('./routes/contacts'));
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contacts", require("./routes/contacts"));
 
+//Server static assets in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 
-const PORT=process.env.PORT||5001;
+  app.get("*", (req, res) => {
+   return  res.sendFile(path.resolve(__direname, "client", "build", "index.html"));
+  });
+}
 
+const PORT = process.env.PORT || 5001;
 
-app.listen(PORT, ()=>console.log(`Server started on port ${PORT} `));
-
+app.listen(PORT, () => console.log(`Server started on port ${PORT} `));
