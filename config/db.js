02931bc7a28f0212a6072a3989c54558b3df9f2c
Sachin <algorithms2019@gmail.com>
2021-06-03 13:29:18 +0530

    removed some info

--- config/db.js
+++ config/db.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 const config = require("config");
-// const DB =
-//   "mongodb+srv://sacmongo:Sacdb@786@test.ukunm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
 const db =config.get('mongoURI');
 // console.log(db);
 
